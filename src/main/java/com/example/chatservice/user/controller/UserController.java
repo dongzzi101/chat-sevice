0c605957a628313c8f5b3d86cce26a92d92425ef
dongwook <dongzzi101@gmail.com>
2025-08-11 02:23:47 +0900

    setup

---
+++ src/main/java/com/example/chatservice/user/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.example.chatservice.user.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/example/chatservice/user/entity/User.java
@@ -0,0 +1,18 @@
+package com.example.chatservice.user.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
