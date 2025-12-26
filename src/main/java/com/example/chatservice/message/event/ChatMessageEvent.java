0d27d83e191932a033e62ebfb99dc10033d4a4ea
dongwookkim <dongzzi101@gmail.com>
2025-12-26 10:28:10 +0900

    kafka

---
+++ src/main/java/com/example/chatservice/message/event/ChatMessageEvent.java
@@ -0,0 +1,29 @@
+package com.example.chatservice.message.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * Kafka로 전송되는 채팅 메시지 이벤트
+ * 서버 간 메시지 전달을 위한 DTO
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessageEvent {
+
+    private Long messageId;
+    private Long senderId;
+    private String content;
+    private Long chatRoomId;
+    private LocalDateTime sentAt;
+}
+
+
+
+
+
+
