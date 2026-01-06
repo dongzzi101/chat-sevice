499902a3edd7f93b9d46fecaf64aa94bfd25a7cd
dongwookkim <dongzzi101@gmail.com>
2026-01-06 18:17:40 +0900

    ChatTypeTest

--- src/main/java/com/example/chatservice/chat/entity/ChatType.java
+++ src/main/java/com/example/chatservice/chat/entity/ChatType.java
@@ -1,9 +1,18 @@
 package com.example.chatservice.chat.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ChatType {
 
-    DIRECT,
-    GROUP,
-    IM
+    DIRECT("일대일방"),
+    GROUP("그룹방"),
+    IM("자신한테 보내는 메시지방");
+
+    private final String text;
+
+
 
 }
